1159cbf54a2cb2bd602a68805cdddba3ef52c7ce
sailist <sailist@outlook.com>
2020-04-14 12:15:51 +0800

    添加了ScreenStr对Pycharm的支持

--- thexp/utils/screen.py
+++ thexp/utils/screen.py
@@ -24,12 +24,12 @@ import time
 
 import numpy as np
 
-
+import os
 def get_consolo_width():
     return shutil.get_terminal_size().columns-1
 
 def support_multiline():
-    if "jupyter_core" in sys.modules or shutil.get_terminal_size((0,0)).columns == 0:
+    if "jupyter_core" in sys.modules or shutil.get_terminal_size((0,0)).columns == 0 or "PYCHARM_HOSTED" in os.environ:
         return True
     else:
         return False
